74f6c7c2413d811586b800a9d53add271ef0bbbe
Rocket Codester <45156318+RocketCodester@users.noreply.github.com>
2018-12-23 19:42:50 -0600

    Add auto-loading

--- index.php
+++ index.php
@@ -3,8 +3,7 @@
 //Don't worry about this bit
 ini_set('display_errors', 1);
 
-include './src/HTTP/Client.php';
-include './src/Twitter/Client.php';
+include './bootstrap.php';
 
 var_dump(new HTTP\Client);
 var_dump(new Twitter\Client);
